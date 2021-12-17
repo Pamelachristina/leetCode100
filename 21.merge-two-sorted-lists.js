72ca2c86b140cd19e097e802f0d84dd71735e71f
psanchezher <pamela_sanchezhernandez@intuit.com>
2021-12-16 22:18:41 -0800

    solved 21

--- 21.merge-two-sorted-lists.js
+++ 21.merge-two-sorted-lists.js
@@ -18,7 +18,35 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    
+    const dummyHead = new ListNode();
+    let current = dummyHead;
+
+    while(list1 && list2) {
+        if(list1.val < list2.val){
+            current.next = list1
+            list1 = list1.next
+        }else {
+            current.next = list2
+            list2 = list2.next
+        }
+        current = current.next
+    }
+
+    while(list1){
+        current.next = list1
+        list1 = list1.next
+        current = current.next
+    }
+
+    while(list2){
+        current.next = list2
+        list2 = list2.next
+        current = current.next
+    }
+
+    return dummyHead.next
+
+
 };
 // @lc code=end
 
---
+++ AceFrontEndInterview/ArraysChallenge4.js
@@ -0,0 +1,4 @@
+//Challenge 4: Array of Products of All Elements
+//Given an array, return an array where each index stores the product 
+//of all numbers in the array except the number at the index itself.
+
