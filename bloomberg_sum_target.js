cad4ae488b1e8948d2f484c8c72ddb1f9b6a0f78
Fuzzy Hair <pamelasanchezhernandez@Pamelas-MacBook-Pro.local>
2023-11-27 16:57:04 -0800

    added new problem 88.merge sorted array

---
+++ bloomberg_sum_target.js
@@ -0,0 +1,14 @@
+/* Given an array of unique positive integers and a target value, print all pairs whoes sum is equal to the target
+example 1
+            numbers = [3,3,2,6,8,5],
+            target = 8
+            solution = (3,5), (2,6)    
+            
+*/
+
+function sumOfPairs (nums, ){
+
+}
+
+
+
